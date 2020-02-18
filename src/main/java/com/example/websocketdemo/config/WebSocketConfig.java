55b9a8536a6901990ad664d8ec1eab0db852e2d4
Aditya Panikkar <aditya@tradewindow.io>
2020-02-19 10:24:12 +1300

    Adding code for broadcasting user destination for unresolved destinations

--- src/main/java/com/example/websocketdemo/config/WebSocketConfig.java
+++ src/main/java/com/example/websocketdemo/config/WebSocketConfig.java
@@ -16,17 +16,17 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
         registry.setApplicationDestinationPrefixes("/app");
-        registry.enableSimpleBroker("/topic");   // Enables a simple in-memory broker
+        //registry.enableSimpleBroker("/topic");   // Enables a simple in-memory broker
 
 
         //   Use this for enabling a Full featured broker like RabbitMQ
 
-        /*
+
         registry.enableStompBrokerRelay("/topic")
                 .setRelayHost("localhost")
                 .setRelayPort(61613)
                 .setClientLogin("guest")
                 .setClientPasscode("guest");
-        */
+               // .setUserDestinationBroadcast("topic/unresolved-user-dest");
     }
 }
